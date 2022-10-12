7d5f3d7a619b14a848ff333fb64ffca277b526d5
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-10-12 16:33:33 -0500

    About

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 
 function Nav() {
+  const categories = [
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ];
   return (
     <header>
       <h2>
@@ -20,6 +33,11 @@ function Nav() {
           <li>
             <span>Contact</span>
           </li>
+          {categories.map((category) => (
+            <li key={category.name}>
+              <span>{category.name}</span>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
